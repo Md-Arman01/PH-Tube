725c2b666b58103ac8c906a9d3b8c6a6596e2523
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-08-31 22:06:11 +0600

    added no data views

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const handleCategories = async() =>{
         // console.log(element)
         const div = document.createElement('div');
         div.innerHTML = `
-        <input  onclick="categoriesClick('${element.category_id}')" class="btn px-6 normal-case" type="radio" name="options" aria-label="${element.category}" />
+        <input  onclick="categoriesClick('${element.category_id}')" class="btn px-6 normal-case" type="radio" name="options" aria-label="${element.category}"/>
         
         `
         categoriesContainer.appendChild(div)
@@ -25,13 +25,21 @@ const categoriesClick =async (id) =>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const idData = await response.json()
     const idDataArr = idData.data
-    // console.log(idDataArr)
+    console.log(idData)
+
+    // no data condition
+    const noData = document.getElementById('no-data')
+    if(idData.status === false){
+      noData.classList.remove('hidden')
+    }else{
+      noData.classList.add('hidden')
+    }
 
     const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML= ''
 
     idDataArr.forEach((element) =>{
-        console.log(element)
+        // console.log(element)
         const div = document.createElement('div')
         div.innerHTML=`
         <div class="card">
@@ -72,3 +80,4 @@ const categoriesClick =async (id) =>{
 
 
 handleCategories()
+categoriesClick('1000')
