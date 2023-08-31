bdf668f51eb0f0dcde257a93734564010ae521a5
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-08-31 20:31:21 +0600

    added categories name

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log('added js')
+// categories 
+const handleCategories = async() =>{
+    const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const categoriesData = await response.json()
+    const categoriesDataArr = categoriesData.data
+    console.log(categoriesDataArr)
+
+
+    const categoriesContainer = document.getElementById('categories-container');
+    categoriesDataArr.forEach(element => {
+        console.log(element)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <input class="btn px-6 normal-case" type="radio" name="options" aria-label="${element.category}" />
+        `
+        categoriesContainer.appendChild(div)
+
+    });
+
+
+
+
+
+
+
+
+
+}
+handleCategories()
