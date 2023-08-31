b5ca2291ac8c959ea5b8a8c6876a228aeea3e6c9
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-08-31 20:57:05 +0600

    id to data load complete

--- js/index.js
+++ js/index.js
@@ -3,27 +3,39 @@ const handleCategories = async() =>{
     const response = await fetch('https://openapi.programming-hero.com/api/videos/categories')
     const categoriesData = await response.json()
     const categoriesDataArr = categoriesData.data
-    console.log(categoriesDataArr)
 
 
     const categoriesContainer = document.getElementById('categories-container');
     categoriesDataArr.forEach(element => {
-        console.log(element)
+        // console.log(element)
         const div = document.createElement('div');
         div.innerHTML = `
-        <input class="btn px-6 normal-case" type="radio" name="options" aria-label="${element.category}" />
+        <input  onclick="categoriesClick('${element.category_id}')" class="btn px-6 normal-case" type="radio" name="options" aria-label="${element.category}" />
+        
         `
         categoriesContainer.appendChild(div)
+
 
     });
 
 
+}
 
-
-
-
-
+const categoriesClick =async (id) =>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const idData = await response.json()
+    console.log(idData)
+    
 
 
 }
+
+
+
+
+
+
+
+
+
 handleCategories()
